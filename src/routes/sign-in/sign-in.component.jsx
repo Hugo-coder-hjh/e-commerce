3383020f8fe76957cd4f0736bde2ef4660191501
JIAHAO HE <hjh18038806482@163.com>
2022-07-13 21:05:58 -0400

    created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,36 @@
-import {signInWithGooglePopup} from '../../utils/firebase/firebase.utils';
-import { createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import {
+auth,
+signInWithGooglePopup,
+createUserDocumentFromAuth,
+// signInWithGoogleRedirect,
+} from '../../utils/firebase/firebase.utils';
+
+import {useEffect} from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { async } from '@firebase/util';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 
 
 const SignIn = () => {
+//    useEffect(async () => {
+//     const response = await getRedirectResult(auth);
+//     console.log(response);
+//    }, [])
+    
+
     //when make call to database, it is an asynchronous operation
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
-
 
     return (
         <div>
           <h1>Sign In Page</h1>
           <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+          <SignUpForm />
+          {/* <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
         </div>
     )
 }
